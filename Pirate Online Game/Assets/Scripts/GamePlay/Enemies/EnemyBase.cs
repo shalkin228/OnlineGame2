09d523925d0bfa59d69ab04c5864ce62e69c2216
shalkin228 <sim.tallinn@gmail.com>
2021-07-09 22:57:00 +0300

    update

--- Pirate Online Game/Assets/Scripts/GamePlay/Enemies/EnemyBase.cs
+++ Pirate Online Game/Assets/Scripts/GamePlay/Enemies/EnemyBase.cs
@@ -6,14 +6,22 @@ using UnityEngine.AI;
 
 public class EnemyBase : MonoBehaviour
 {
-    public int hp;
+    [HideInInspector] public int currentHp = 0;
+
+    [SerializeField] private int maxHp;
 
     private NavMeshAgent agent;
     private PhotonView photonView;
+    private Animator anim;
 
     void Start()
     {
+        currentHp = maxHp;
+
         photonView = GetComponent<PhotonView>();
+
+        anim = GetComponent<Animator>();
+
         agent = GetComponent<NavMeshAgent>();
         agent.updateRotation = false;
         agent.updateUpAxis = false;
@@ -28,17 +36,17 @@ public class EnemyBase : MonoBehaviour
             if (FindObjectsOfType<PlayerMovement>() == null)
                 return;
 
-            foreach(var player in FindObjectsOfType<PlayerMovement>())
+            foreach (var player in FindObjectsOfType<PlayerMovement>())
             {
                 players.Add(player.transform);
             }
 
             float minDistance = Vector3.Distance(players[0].position, transform.position);
-            int minDistancePlayerIndex = 0;                                                           
+            int minDistancePlayerIndex = 0;
             int currentIterationIndex = 0;
-            foreach(var player in players)
+            foreach (var player in players)
             {
-                if(Vector3.Distance(player.position, transform.position) < minDistance)
+                if (Vector3.Distance(player.position, transform.position) < minDistance)
                 {
                     minDistance = Vector3.Distance(player.position, transform.position);
                     minDistancePlayerIndex = currentIterationIndex;
@@ -46,8 +54,54 @@ public class EnemyBase : MonoBehaviour
                 currentIterationIndex++;
             }
 
+            if (Vector3.Distance(players[minDistancePlayerIndex].position, transform.position) > 50)
+            {
+                agent.isStopped = true;
+
+            }
+
+
             agent.SetDestination(players[minDistancePlayerIndex].position);
+
+            Vector3 playerLocalPos = transform.InverseTransformPoint(players[minDistancePlayerIndex].position);
+            if (playerLocalPos.x >= 0)
+            {
+                Flip(Direction.Right);
+            }
+            else
+            {
+                Flip(Direction.Left);
+            }
+
+            if (agent.velocity != Vector3.zero)
+            {
+                anim.SetBool("IsRunning", true);
+            }
+            else
+            {
+                anim.SetBool("IsRunning", false);            
+            }
+        }
+
+    }
+
+    private void Flip(Direction dir)
+    {
+        if(dir == Direction.Left)
+        {
+            foreach(Transform child in transform.GetChild(0))
+            {
+                child.GetComponent<SpriteRenderer>().flipX = true;
+            }
+        }
+        else
+        {
+            foreach (Transform child in transform.GetChild(0))
+            {
+                child.GetComponent<SpriteRenderer>().flipX = false;
+            }
         }
     }
 
 }
+public enum Direction { Right, Left }
